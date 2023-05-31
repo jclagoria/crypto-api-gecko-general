9b6f23dfa1d2ca65f1bfc086ef203590d61ff5a6
jclagoria <lagoria79@gmail.com>
2023-05-30 23:37:30 -0300

    #1 global api - global data

--- src/main/java/ar/com/api/general/router/RouterConfig.java
+++ src/main/java/ar/com/api/general/router/RouterConfig.java
@@ -1,6 +1,7 @@
 package ar.com.api.general.router;
 
-import ar.com.api.general.handler.ApiStatusHandler;
+import ar.com.api.general.handler.GlobalApiHandler;
+import ar.com.api.general.handler.HealthApiHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,23 +11,36 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 
 
 @Configuration
-public class ApiRouter {
+public class RouterConfig {
 
  @Value("${coins.baseURL}")
  private String URL_SERVICE_API;
 
  @Value("${coins.healthAPI}")
  private String URL_HEALTH_GECKO_API;
+
+ @Value("${coins.global}")
+ private String URL_GLOBAL_GECKO_API;
  
  @Bean
- public RouterFunction<ServerResponse> route(ApiStatusHandler handler) {
+ public RouterFunction<ServerResponse> route(HealthApiHandler handler) {
 
   return RouterFunctions
             .route()
             .GET(URL_SERVICE_API + URL_HEALTH_GECKO_API, 
                         handler::getStatusServiceCoinGecko)
             .build();
+ }
 
+ @Bean
+ public RouterFunction<ServerResponse> routeGlobal(GlobalApiHandler handler) {
+
+  return RouterFunctions
+          .route()
+          .GET(
+                  URL_SERVICE_API + URL_GLOBAL_GECKO_API,
+                  handler::getGlobalDataFromGeckoApi)
+          .build();
  }
 
 }
