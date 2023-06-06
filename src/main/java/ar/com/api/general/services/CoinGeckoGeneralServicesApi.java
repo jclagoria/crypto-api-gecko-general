c1cc047aa00051f807fd770f535be7012f93c339
jclagoria <lagoria79@gmail.com>
2023-06-05 22:29:22 -0300

    get decentralized finance defi from gecko coin

--- src/main/java/ar/com/api/general/services/CoinGeckoGeneralServicesApi.java
+++ src/main/java/ar/com/api/general/services/CoinGeckoGeneralServicesApi.java
@@ -1,5 +1,6 @@
 package ar.com.api.general.services;
 
+import ar.com.api.general.model.DecentralizedFinance;
 import ar.com.api.general.model.Global;
 
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +16,9 @@ public class CoinGeckoGeneralServicesApi {
 
     @Value("${api.global}")
     private String URL_GECKO_SERVICE_GLOBAL_API;
+
+    @Value("${api.decentralized}")
+    private String URL_GECKO_SERVICE_DECENTRALIZED_FINANCE_DEFI;
     private WebClient webClient;
 
     public CoinGeckoGeneralServicesApi(WebClient webClient) {
@@ -33,5 +37,16 @@ public class CoinGeckoGeneralServicesApi {
                 .doOnError(throwable -> log.error("The service is unavailable!", throwable));
     }
 
+    public Mono<DecentralizedFinance> getDecentralizedFinance() {
+
+        log.info("n getGlobalData - Calling DecentralizedFinance");
+
+        return webClient
+                .get()
+                .uri(URL_GECKO_SERVICE_DECENTRALIZED_FINANCE_DEFI)
+                .retrieve()
+                .bodyToMono(DecentralizedFinance.class)
+                .doOnError(throwable -> log.error("The service is unavailable!", throwable));
+    }
 
 }
