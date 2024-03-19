b02fb0eccb8b07a2193347a7b20da23e5e350e16
jclagoria <lagoria79@gmail.com>
2024-03-18 22:46:00 -0300

    Changes for fetch CoinGecko Search Trending, add correct dependencies, add test for service and handler

--- src/main/java/ar/com/api/general/handler/SimpleApiHandler.java
+++ src/main/java/ar/com/api/general/handler/SimpleApiHandler.java
@@ -1,7 +1,5 @@
 package ar.com.api.general.handler;
 
-import ar.com.api.general.dto.SimplePriceFilterDTO;
-import ar.com.api.general.dto.TokenPriceByIdDTO;
 import ar.com.api.general.enums.ErrorTypeEnum;
 import ar.com.api.general.exception.ApiClientErrorException;
 import ar.com.api.general.handler.utils.MapperHandler;
@@ -14,8 +12,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
-
-import java.util.Map;
 
 @Component
 @Slf4j
@@ -51,29 +47,23 @@ public class SimpleApiHandler {
     }
 
     public Mono<ServerResponse> getSimplePriceTokenByIDFromCoinGeckoApi(ServerRequest serverRequest) {
-        log.info("Fetching simple price token by ID from CoinGecko API");
+        log.info("Fetching simple price token by ID from CoinGecko API {}", serverRequest.path());
 
         return Mono.just(serverRequest)
-                .flatMap(req -> {
-                    TokenPriceByIdDTO tokenPriceByIdDTO = createTokenPriceByIdDTO(serverRequest);
-                    return ServerResponse.ok().body(coinGeckoSimpleApiService.getSimplePriceTokenById(tokenPriceByIdDTO), Map.class);
-                })
-                .switchIfEmpty(ServerResponse.badRequest().build());
-    }
-
-    private TokenPriceByIdDTO createTokenPriceByIdDTO(ServerRequest sRequest) {
-        return TokenPriceByIdDTO.builder()
-                .ids(sRequest.pathVariable("ids"))
-                .contractAddresses(sRequest.queryParam("contractAddress")
-                        .orElseThrow(() -> new IllegalArgumentException("Contract address parameter is required")))
-                .vsCurrencies(sRequest.queryParam("vsCurrencies")
-                        .orElseThrow(() -> new IllegalArgumentException("vsCurrencies parameter is required")))
-                .includeMarketCap(sRequest.queryParam("includeMarketCap"))
-                .include24hrVol(sRequest.queryParam("include24hrVol"))
-                .include24hrChange(sRequest.queryParam("include24hrChange"))
-                .includeLastUpdatedAt(sRequest.queryParam("includeLastUpdatedAt"))
-                .precision(sRequest.queryParam("precision"))
-                .build();
+                .flatMap(MapperHandler::createTokenPriceByIdDTOFromRequest)
+                .flatMap(validatorOfCTOComponent::validation)
+                .flatMap(coinGeckoSimpleApiService::getSimplePriceTokenById)
+                .flatMap(mapObject -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .bodyValue(mapObject))
+                .doOnSubscribe(subscription -> log.info("Retrieving Map Object from CoinGecko API"))
+                .switchIfEmpty(ServerResponse.notFound().build())
+                .onErrorResume(error -> Mono.error(
+                        new ApiClientErrorException(
+                                "An unexpected error occurred in getSimplePriceTokenByIDFromCoinGeckoApi",
+                                ErrorTypeEnum.API_SERVER_ERROR,
+                                HttpStatus.INTERNAL_SERVER_ERROR)
+                ));
     }
 
 }
--- src/test/java/ar/com/api/general/handler/SimpleApiHandlerTest.groovy
+++ src/test/java/ar/com/api/general/handler/SimpleApiHandlerTest.groovy
@@ -1,6 +1,7 @@
 package ar.com.api.general.handler
 
 import ar.com.api.general.dto.SimplePriceFilterDTO
+import ar.com.api.general.dto.TokenPriceByIdDTO
 import ar.com.api.general.entity.DataMapTest
 import ar.com.api.general.enums.ErrorTypeEnum
 import ar.com.api.general.exception.ApiClientErrorException
@@ -51,7 +52,7 @@ class SimpleApiHandlerTest extends Specification {
                 .verifyComplete()
     }
 
-    def "GetSimplePriceFromCoinGeckoApi  returns not found when the service return from the API Service"() {
+    def "GetSimplePriceFromCoinGeckoApi returns not found when the service return from the API Service"() {
         given: "Mocked ServerResponse and Mocked ValidatorComponent and Mocked ApiService and Mocked SimpleApiHandler to return Not Found"
         def filterDTO = Instancio.create(SimplePriceFilterDTO.class)
         serverRequestMock.queryParam(_ as String) >> Optional.of("bitcoin")
@@ -85,6 +86,70 @@ class SimpleApiHandlerTest extends Specification {
                             actualError.getErrorTypeEnum() == ErrorTypeEnum.API_SERVER_ERROR &&
                             actualError.getHttpStatus().is5xxServerError() &&
                             actualError.getMessage() == "An unexpected error occurred in getSimplePriceFromCoinGeckoApi"
+                }
+                .verify()
+    }
+
+    def "GetSimplePriceTokenByIDFromCoinGeckoApi return successfully a ServerResponse with a HttpStatus Ok"() {
+        given: "Mocked ServerResponse and Mocked ValidatorComponent and Mocked ApiService and Mocked SimpleApiHandler"
+        def filterDTO = Instancio.create(TokenPriceByIdDTO.class)
+        def expectedObject = Instancio.ofMap(String.class, DataMapTest.class).size(5).create()
+        serverRequestMock.pathVariable(_ as String) >> Instancio.create(String.class)
+        serverRequestMock.queryParam(_ as String) >> Optional.of(Instancio.create(String.class))
+        serverRequestMock.queryParam(_ as String) >> Optional.of(Instancio.create(String.class))
+        validatorMock.validation(_) >> Mono.just(filterDTO)
+        coinGeckoSimpleApiServiceMock.getSimplePriceTokenById(_ as TokenPriceByIdDTO) >> Mono.just(expectedObject)
+
+        when: "GetSimplePriceTokenByIDFromCoinGeckoApi is called and return successfully ServerRequest"
+        def actualResponseObject = apiHandler
+                .getSimplePriceTokenByIDFromCoinGeckoApi(serverRequestMock)
+
+        then: "It returns a ServerResponse with HttpResponse 200 Ok"
+        StepVerifier.create(actualResponseObject)
+                .expectNextMatches{actualResponse ->
+                    actualResponse.statusCode().is2xxSuccessful() &&
+                            actualResponse.headers().getContentType() == MediaType.APPLICATION_JSON
+                }
+                .verifyComplete()
+    }
+
+    def "GetSimplePriceTokenByIDFromCoinGeckoApi returns not found when the service return from the API Service"() {
+        given: "Mocked ServerResponse and Mocked ValidatorComponent and Mocked ApiService and Mocked SimplePriceTokenByID to return Not Found"
+        def filterDTO = Instancio.create(TokenPriceByIdDTO.class)
+        serverRequestMock.pathVariable(_ as String) >> Instancio.create(String.class)
+        serverRequestMock.queryParam(_ as String) >> Optional.of(Instancio.create(String.class))
+        serverRequestMock.queryParam(_ as String) >> Optional.of(Instancio.create(String.class))
+        validatorMock.validation(_) >> Mono.just(filterDTO)
+        coinGeckoSimpleApiServiceMock.getSimplePriceTokenById(_ as TokenPriceByIdDTO) >> Mono.empty()
+
+        when: "GetSimplePriceTokenByIDFromCoinGeckoApi is called and return a not found ServerResponse"
+        def actualResponseNotFound = apiHandler
+                .getSimplePriceTokenByIDFromCoinGeckoApi(serverRequestMock)
+
+        then: "It returns a Not Found Response and validate"
+        StepVerifier.create(actualResponseNotFound)
+                .expectNextMatches {actualResponse ->
+                    actualResponse.statusCode() == HttpStatus.NOT_FOUND
+                }
+                .verifyComplete()
+    }
+
+    def "GetSimplePriceTokenByIDFromCoinGeckoApi handles and error gracefully"() {
+        given: "Mocked ServerRequest and ApiService return Error Scenario"
+        coinGeckoSimpleApiServiceMock.getSimplePriceTokenById(_ as TokenPriceByIdDTO) >>
+                Mono.error(new RuntimeException("An error Occurred"))
+
+        when: "GetSimplePriceTokenByIDFromCoinGeckoApi is called and return successfully ServerRequest"
+        def actualErrorResponse = apiHandler
+                .getSimplePriceTokenByIDFromCoinGeckoApi(serverRequestMock)
+
+        then: "It handles the error and validate if is an Internal Server Error"
+        StepVerifier.create(actualErrorResponse)
+                .expectErrorMatches{actualError ->
+                    actualError instanceof ApiClientErrorException &&
+                            actualError.getErrorTypeEnum() == ErrorTypeEnum.API_SERVER_ERROR &&
+                            actualError.getMessage() == "An unexpected error occurred in getSimplePriceTokenByIDFromCoinGeckoApi" &&
+                            actualError.getHttpStatus().is5xxServerError()
                 }
                 .verify()
     }
